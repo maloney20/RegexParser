d5c709f273224d6466d81d5d49021faae79cb818
PatrickMaloney <maloney20@up.edu>
2019-12-01 22:40:03 -0800

    working kinda

--- regexperiment.py
+++ regexperiment.py
@@ -1,102 +1,143 @@
 import sys
-
-exp = sys.argv[1]
-
+statenum = 0
 def isAlpha(s):
 	for ch in s:
 		if ord(ch) not in range(65, 90) and ord(ch) not in range(97, 122):
 			return False
 	return True
 
-def isOperator(s)
+def isOperator(s):
 	return s == "*" or s == "|"
 
-class Expression:
-
-	class State:
-		def __init__(self, accept):
+class State:
+		def __init__(self, num, accept):
+			global statenum
+			statenum += 1
+			self.num = num
 			self.accept = accept
 			self.out = []
 
-	class Fragment:
-		def __init__(self, start, end):
-			self.start = start
-			self.end = end
+class Fragment:
+	def __init__(self, start, end):
+		self.start = start
+		self.end = end
+		self.connectors = []
 
 
+class Expression:
 	def __init__(self, exp):
 		self.exp = exp
 		self.accept = []
-		self.start = self.buildNFA()
+		self.buildNFA()
+
+	def showNfa(self, state, shownStates):
+		print(state.num)
+		for t in state.out:
+			print(t[0], t[1].num, t[1].accept)
+		shownStates.append(state.num)
+		for t in state.out:
+			if t[1].num not in shownStates:
+				self.showNfa(t[1], shownStates)
 
 	def buildNFA(self):
 		fragments = []
 		for i in range(len(self.exp)):
 			ch = self.exp[i]
 			if isAlpha(ch):
-				s = State(False)
-				e = State(True)
+				s = State(statenum, False)
+				e = State(statenum, False)
 				s.out.append((ch, e))
-				e.out.append(("", "next"))
-				fragments.append(Fragment(s, [e]))
+				#e.out.append(("", "next"))
+				frag = Fragment(s, [e])
+				frag.connectors.append(e)
+
+				fragments.append(frag)
 			elif ch == "*":
 				f = fragments.pop()
-				s = State(True)
+				s = State(statenum, False)
+				frag = Fragment(s, [s])
 				s.out.append(("", f.start))
-				s.out.append(("", "next"))
+				#s.out.append(("", "next"))
+				frag.connectors.append(s)
+
 				for end in f.end:
 					end.accept = False
-					for out in end.out:
-						out.append(("", s))
-				fragments.append(Fragment(s, [s]))
+					end.out.append(("", s))
+
+				fragments.append(frag)
 			elif ch == "|":
 				f = fragments.pop()
-				s = State(False)
-				s.out.append("", f.start)
-				s.out.append("", "next")
+				s = State(statenum, False)
+				s.out.append(("", f.start))
 				for end in f.end:
+					end.accept = True
 					for out in end.out:
-						if out[1] == "next":
-							end.out.remove(out)
-				fragments.append(Fragment((s, f.end)))
+						out.out = []
+				frag = Fragment(s, f.end)
+				frag.connectors.append(s)
+				fragments.append(frag)
 			elif ch == "(":
 				pstack = ["("]
 				closeIndex = i
-				for j in range(i, len(self.exp)):
+				for j in range(i+1, len(self.exp)):
 					if self.exp[j] == "(":
 						pstack.append("(")
 					elif self.exp[j] == ")":
 						pstack.pop()
-					elif pstack.isEmpty():
+					elif len(pstack)==0:
 						closeIndex = j
 						break
-				parenExp = Expression(self.exp[i:closeIndex]).buildNFA()
+				parenExp = Expression(self.exp[i+1:closeIndex-1]).buildNFA()
 				fragments.append(parenExp)
-
 
 		#put all fragments together
 		endStates = []
 		for i in range(len(fragments)):
 			if i == len(fragments)-1:
 				endStates = fragments[i].end
+				for end in endStates:
+					#print(end.num, "accepts")
+					end.accept = True
 				break
 			frag = fragments[i]
-			for end in frag.end:
-				end.out.append(("", fragments[i+1].start))
-
+			for con in frag.connectors:
+				con.out.append(("", fragments[i+1].start))
 
 		self.start = fragments[0].start
-		return Fragment((fragments[0].start, endStates))
+		return Fragment(fragments[0].start, endStates)
+
 
 
 	#simulate s on the nfa
 	def match(self, s):
-		possiblestates = [self.start]
+		#.showNfa(self.start, [])
+		possibleStates = [self.start]
+		for transition in self.start.out:
+			if transition[0] == "":
+				possibleStates.append(transition[1])
 		for ch in s:
-			for state in possiblestates:
+			possibleCopy = possibleStates.copy()
+			for state in possibleStates:
 				for out in state.out:
-					if out[1] == ch:
 
+					if out[0] == ch or out[0] == "":
+						possibleCopy.append(out[1])
+				possibleCopy.remove(state)
+			possibleStates = possibleCopy
+		for state in possibleStates:
+			if state.accept:
+				return True
+		return False
+
+
+def main():
+	exp = Expression("a|b")
+	print("-------------------------------------")
+	print(str(exp.match("a")))
+	print(str(exp.match("b")))
+	print(str(exp.match("c")))
+
+main()
 
 
 
